dbd049062907b9a30491004b8929d3dd76470c5a
Benjamin Dass <benrdass@gmail.com>
2023-06-19 18:04:28 -0600

    added SwaggerUI for API documentation

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -24,14 +24,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Internal
     "account",
     "server",
+    # External
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
